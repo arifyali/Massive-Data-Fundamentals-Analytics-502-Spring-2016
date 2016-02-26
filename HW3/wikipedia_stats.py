a687aaeeee0cbbf33eb352efebf5050352e0965f
Arif Ali <arifyuali@gmail.com>
2016-02-26 04:36:46 +0000

    update to wiki stats

--- HW3/wikipedia_stats.py
+++ HW3/wikipedia_stats.py
@@ -12,25 +12,25 @@ import sys
 from weblog import Weblog  # imports class defined in weblog.py
 
 class wikipedia_stats(MRJob):
-	def mapper(self, _, line):
-		article = line.split('\t')
-		yield article[1], 1
+    def mapper(self, _, line):
+	article = line.split('\t')
+	yield article[1], 1
 
-	def reducer(self, key, value):
-		yield key, sum(value)
+    def reducer(self, key, value):
+	yield key, sum(value)
 
-	def mapper_two(self, key, value):
-			yield "WikiMods", (key, value)
+    def mapper_two(self, key, value):
+	yield "WikiMods", (key, value)
 
-	def reducer_two(self, key, value):
-			yield value[0], value[1]
+    def reducer_two(self, key, value):
+	yield value[0], value[1]
 
-	def steps(self):
-		return [
-			MRStep(mapper=self.mapper,
-				   reducer=self.reducer),
-			MRStep(mapper=self.mapper_two,
-				   reducer=self.reducer_two) ]	
+    def steps(self):
+	return [
+            MRStep(mapper=self.mapper,
+	    reducer=self.reducer),
+	    MRStep(mapper=self.mapper_two,
+	    reducer=self.reducer_two) ]	
 
 if __name__ == "__main__":
     wikipedia_stats.run()
